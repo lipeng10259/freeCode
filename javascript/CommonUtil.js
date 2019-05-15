bf2de96f949cb2effae7886afda70c9668f4c35c
liepng10259 <1025917773@qq.com>
2019-05-15 23:22:55 +0800

    dd

--- javascript/CommonUtil.js
+++ javascript/CommonUtil.js
@@ -82,3 +82,23 @@
         }
 
     }
+
+
+    BH.EventUtil = {
+        addHandler : function (element , type , handler) {
+            if (element.addEventListener){
+                element.addEventListener(type , handler , false)
+            }else if(element.attachEvent){
+                element.attachEvent('on' + type , handler)
+            }
+        },
+        removeHandler : function (element , type , handler) {
+            if (element.removeEventListener){
+                element.removeEventListener(type , handler , false)
+            }else if(element.detachEvent){
+                element.detachEvent('on' + type , handler)
+            }
+        },
+
+
+    }
